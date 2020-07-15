ccd1676bd43cf8e5ddd888b772940d05aa24c6ab
alalufcan <alalufcan@gmail.com>
2020-07-15 13:08:57 -0400

    final

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,58 +1,76 @@
-# In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. 
-# You will give a set of financial data called budget_data.csv. The dataset is composed of two columns: Date and Profit/Losses. 
-# (Thankfully, your company has rather lax standards for accounting so the records are simple.)
-
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
-# (check)The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The average of the changes in "Profit/Losses" over the entire period
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in losses (date and amount) over the entire period
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $38382578
-# Average  Change: $-2315.12
-# Greatest Increase in Profits: Feb-2012 ($1926159)
-# Greatest Decrease in Profits: Sep-2013 ($-2196167)
-
 import os
 import csv
-import datetime
-pybank_csv = os.path.join('pybank.csv')
 
-num=[]
-total=0
-average_change = []
-greatest_increase = []
-greatest_decrease = []
-with open(pybank_csv, newline='') as csvfile:
-
-    pybank_csv = csv.reader(csvfile, delimiter=',')
+# Path to collect data from the Resources folder
+budget_data = os.path.join('..','pyBank', 'Resources', 'budget_data.csv')
 
 
-    def number_of_months(pybank_csv):
-        dates = str(pybank_csv[0])
-        pl = int(pybank_csv[1])
-    for i in pybank_csv:
-        num.append(i)
+# Read in the CSV file
+with open(budget_data, 'r') as csvfile:
+    
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Skip header row
+    csv_header = next(csvreader, None)
+
+    # prepare lists
+    all_months = []
+    PandL_val = []
+    change_val = []
+
+    # Loop through the data
+    for row in csvreader:
         
-    print(len(num)-1)
+        #insert csv values into lists
+        all_months.append(row[0])
+        PandL_val.append(int(row[1]))
 
-# profitandloss=0
-#     for i in pybank_csv[1]:
-#         profitandloss=profitandloss+i
+        #generate list of changes from month to month
+        if len(PandL_val) >= 2:
+          change_val.append((PandL_val[len(PandL_val)-1] - PandL_val[len(PandL_val)-2]))
+        
+    #calculate & format totals
+    total_months = len(all_months)
+    total_PandL = '${:,}'.format(sum(PandL_val))
 
-#     print(profitandloss)
+    #calculate & format average change
+    avg_change = sum(change_val) / total_months
+    avg_change = '${:,.2f}'.format(avg_change)
+    
+    #find & format max values from list
+    max_month = PandL_val.index(max(PandL_val))
+    max_month = all_months[max_month]
+    max_val = '${:,}'.format(max(PandL_val))
 
-    for column in pybank_csv:
-        pl= pybank_csv[1]
-            for row in pl:
-                
+    #find & format min values from list
+    min_month = PandL_val.index(min(PandL_val))
+    min_month = all_months[min_month]
+    min_val = '${:,}'.format(min(PandL_val))
 
+#print analysis
+print('Financial Analysis')
+print('------------------------------')
+print(f'Total Months:', total_months)
+print(f'Total:', total_PandL)
+print(f'Average Change:', avg_change)
+print(f'Greatest Increase in Profits:', max_month, max_val)
+print(f'Greatest Decrease in Profits:', min_month, min_val)
 
+#output analysis to file
+output_path = os.path.join('..','pyBank', 'Resources', 'analysis.csv')
 
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
 
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=' ')
 
+    # Write rows
+    csvwriter.writerow(['Financial Analysis'])
+    csvwriter.writerow(['------------------------------'])
+    csvwriter.writerow(['Total Months', total_months])
+    csvwriter.writerow(['Total:', total_PandL])
+    csvwriter.writerow(['Average Change', avg_change])
+    csvwriter.writerow(['Greatest Increase in Profits', max_month, max_val])
+    csvwriter.writerow(['Greatest Decrease in Profits', min_month, min_val])
